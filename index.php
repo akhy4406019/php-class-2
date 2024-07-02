c96ba9d207e9fd4cbcac6123c0c6d045e4066c07
akhy4406019 <149380498+akhy4406019@users.noreply.github.com>
2024-07-02 16:23:14 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    // define("sname","Akhy");
+    // echo sname;
+
+
+    // $sname="Akhy";
+    // echo $sname;
+
+
+    // echo (rand(345000, 9000000));
+
+
+    // $mark=85;
+    // if($mark>=80){
+    //     echo "A+";
+    // }
+
+
+    // $mark=75;
+    // echo $mark>=80? 'A+': 'F';
+
+
+    // https://github.com/akhy4406019/php-class-1
+
+?>
